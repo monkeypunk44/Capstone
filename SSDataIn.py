6065fdbde7500753688bfb84be2e078ee186c31c
AJ <55856227+monkeypunk44@users.noreply.github.com>
2021-11-18 12:34:40 -0400

    Stopped Dupe data
    Need to work on making it so you can have SS and QF with similar data

--- SSDataIn.py
+++ SSDataIn.py
@@ -1,5 +1,7 @@
 import pymongo
 import json
+import re
+from collections import Counter 
 
 def OpenPrices(id):
   file1 = open(id,"r")
@@ -26,38 +28,56 @@ def OpenItems(id):
     a += 1 
   return(Iarray)
 
+def FindCommonWord():
+  CWord = ""
+  most_occur = ""
+  with open('SSItems.txt') as f:
+    text = f.read()
+
+  words = re.compile(r"[\w']+", re.U).findall(text)
+  
+  counts = Counter(words)
+  CWord = counts.most_common(1)[0][0]
+  return CWord 
+
 def makeJSON():
+  
+  Iarray = OpenItems("SSItems.txt")
+  Parray = OpenPrices("SSPrices.txt")
+  count = 0 
+  Products = {}
+  Products['SuperStore'] = []
+  while count < len(Iarray):
 
-    Iarray = OpenItems("SSItems.txt")
-    Parray = OpenPrices("SSPrices.txt")
-    count = 0 
-    Products = {}
-    Products['SuperStore'] = []
-    while count < len(Iarray):
+      Products['SuperStore'].append({
+          'Product': Iarray[count],
+          'Price': Parray[count],
+          'Sale': False,
+          'SalePrice': 0,
+      })
+      count += 1
+  
+  with open('data.json', 'w') as f:
+      json.dump(Products, f) 
 
-        Products['SuperStore'].append({
-            'Product': Iarray[count],
-            'Price': Parray[count],
-            'Sale': False,
-            'SalePrice': 0,
-        })
-        count += 1
-    
-    with open('data.json', 'w') as f:
-        json.dump(Products, f)  
+def ProcessJSON(CWord):
 
-def ProcessJSON():
 
   myclient = pymongo.MongoClient("mongodb+srv://Admin:BvzV5L7bU1psvzz4@cluster0.2wysu.mongodb.net/GoodPricer?retryWrites=true&w=majority")
   mydb = myclient["GoodPricer"]
   mycol= mydb['Stores']
+  print(mycol.find_one({},{"superstore": "Array", "Product": CWord}))
 
-  with open('data.json') as f:
-    file = json.load(f)
+  if(mycol.find_one({},{"superstore": "Array", "Product": CWord}) == None):
 
-  mycol.insert_one(file)
+    with open('data.json') as f:
+      file = json.load(f)
+    
+    mycol.insert_one(file)
+  
 
   myclient.close()
 
 makeJSON()
-ProcessJSON()
+
+ProcessJSON(FindCommonWord())
